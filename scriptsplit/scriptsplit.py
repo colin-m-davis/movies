1971d37bea1ca21d3a2a2b58538e50a09458f094
BellaEsc <97126765+BellaEsc@users.noreply.github.com>
2023-02-21 20:39:06 -0800

    Altered name-line dictionary function
    Now it makes a dictionary with the name as key and a list of their lines as the value, but only captures the first all caps word as the name. still need to fix that

--- scriptsplit/scriptsplit.py
+++ scriptsplit/scriptsplit.py
@@ -7,28 +7,24 @@ script_file = open(os.path.join(cdir, 'megamind.txt'), 'r')
 script = script_file.read()
 script_file.close()
 
-#print(script[2000:2200]) <- uncomment to make sure the script imported right
 
-pars = re.split(r'\n\n+', script, maxsplit=0) #re.S + re.M) <- part of original code (link below) not sure why it was included?
+
+pars = re.split(r'\n\n+', script, maxsplit=0)  # The 'r' at the start of the pattern string 
+# designates a python "raw" string which passes through backslashes without change
 d = {}
 
-# Checking that the split works
-print(pars[100])
-print(len(pars))
-print(pars[1825])
+
 
 # Source: https://stackoverflow.com/questions/57273215/how-to-parse-movie-script-in-a-dictionary
 for p in pars:
     # Capture the name (anchored to the beginning of line and all capitals)
     # and the rest of the paragraph - (.*)
-    regex = re.search(r'^([A-Z]+):(.*)', p, re.S + re.M)
+    regex = re.search(r'^([A-Z]+)(.*)', p, re.S + re.M)
 
     if not regex:  # Avoid calling group() on null results
         continue
 
-    name, txt = regex.group(1, 2) # FIXME: 'NoneType' object has no attribute 'group'
-                                                                         # I think the regex search is bogged
-                                                                         # Will just redo it on my own using the same logic
+    name, txt = regex.group(1, 2) 
 
     # Each sentence as a list item
     if name in d:
@@ -36,7 +32,10 @@ for p in pars:
     else:
         d[name] = txt.strip().split('\n')
 
-pprint.pprint(d)  
+#pprint.pprint(d)  
+print(d.keys())
+
+
 
 # FIXME: this code may not work exactly as intended once regex line is fixed,
 #  since there are parts of the script with no 'speaker' (it's just describing
